818b6a27082fb6e52a09b5428658824d093b6da4
Loc <tranxuanloc.py@gmail.com>
2025-03-13 11:39:34 +0400

    solved 929 using built-in functions

--- 929.go
+++ 929.go
@@ -4,27 +4,11 @@ import "strings"
 
 func numUniqueEmails(emails []string) int {
 	uniqueEmails := make(map[string]struct{})
-loop:
 	for _, email := range emails {
-		plus := false
-		n := len(email)
-		var emailBuilder strings.Builder
-		for i := 0; i < n; i++ {
-			ch := email[i]
-			if ch == '+' {
-				plus = true
-				continue
-			}
-			if ch == '@' {
-				emailBuilder.WriteString(email[i:])
-				uniqueEmails[emailBuilder.String()] = struct{}{}
-				continue loop
-			}
-			if ch == '.' || plus {
-				continue
-			}
-			emailBuilder.WriteByte(ch)
-		}
+		parts := strings.Split(email, "@")
+		local := strings.Split(parts[0], "+")[0]
+		local = strings.ReplaceAll(local, ".", "")
+		uniqueEmails[local+"@"+parts[1]] = struct{}{}
 	}
 	return len(uniqueEmails)
 }
