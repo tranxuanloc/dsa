68e5ac1c392482a4fb04bd481a0a6ab5ae4fb93c
Loc <tranxuanloc.py@gmail.com>
2025-03-18 08:53:13 +0400

    improved 27 by reducing number of copy operations

--- 27.go
+++ 27.go
@@ -1,12 +1,15 @@
 package main
 
 func removeElement(nums []int, val int) int {
-	nextPos := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != val {
-			nums[nextPos] = nums[i]
-			nextPos++
+	left := 0
+	right := len(nums)
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
+		} else {
+			left++
 		}
 	}
-	return nextPos
+	return right
 }
