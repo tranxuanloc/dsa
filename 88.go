dd22c6343b8d03302131c206c64dcd58da74a418
Loc <tranxuanloc.py@gmail.com>
2025-03-18 08:06:04 +0400

    improved 88; shorter

--- 88.go
+++ 88.go
@@ -3,19 +3,13 @@ package main
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	i := m - 1
 	j := n - 1
-	for i > -1 || j > -1 {
-		if i == -1 {
-			nums1[i+j+1] = nums2[j]
-			j--
-		} else if j == -1 {
+	for j > -1 {
+		if i > -1 && nums1[i] > nums2[j] {
 			nums1[i+j+1] = nums1[i]
 			i--
-		} else if nums1[i] < nums2[j] {
-			nums1[i+j+1] = nums2[j]
-			j--
 		} else {
-			nums1[i+j+1] = nums1[i]
-			i--
+			nums1[i+j+1] = nums2[j]
+			j--
 		}
 	}
 }
