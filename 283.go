2e70129f71b37bede3a6b8ee90528849863adf4a
Loc <tranxuanloc.py@gmail.com>
2025-03-19 20:54:15 +0400

    solved 283

---
+++ 283.go
@@ -0,0 +1,14 @@
+package main
+
+func moveZeroes(nums []int) {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != 0 {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	for ; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
